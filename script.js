0b4684fcb2d5cdeee14aa94ab1e58e30089110f0
Vaceslav Ignatuk <vaceslavignatuk@gmail.com>
2021-11-14 21:41:26 +0300

    addTask added

--- script.js
+++ script.js
@@ -11,28 +11,48 @@ const list = [
 		id: 1,
 		task: "wake up",
 		status: DONE,
-		priopity: HIGH_PRIORITY,
+		priority: HIGH_PRIORITY,
 	},
-	{ id: 2, task: "take a shower", status: DONE, priopity: LOW_PRIORITY, },
-	{ id: 3, task: "have breakfast", status: TODO, priopity: LOW_PRIORITY, },
-	{ id: 4, task: "learn JavaScript", status: IN_PROGRESS, priopity: HIGH_PRIORITY, },
-	{ id: 5, task: "have a walk", status: TODO, priopity: LOW_PRIORITY, },
-	{ id: 6, task: "watch stream", status: TODO, priopity: HIGH_PRIORITY, },
+	{ id: 2, task: "take a shower", status: DONE, priority: LOW_PRIORITY, },
+	{ id: 3, task: "have breakfast", status: TODO, priority: LOW_PRIORITY, },
+	{ id: 4, task: "learn JavaScript", status: IN_PROGRESS, priority: HIGH_PRIORITY, },
+	{ id: 5, task: "have a walk", status: TODO, priority: LOW_PRIORITY, },
+	{ id: 6, task: "watch stream", status: TODO, priority: HIGH_PRIORITY, },
 ];
 
+
 /* function deleteTask(task) {
-	delete list[task];
-};
- */
-
-function deleteTask(task) {
-	let result = list.filter(function (item, index) {
-		if (item.task === task) {
-			list.splice(index, 1);
-		};
+	let taskIndex = list.findIndex(function (item) {
+		return item.task === task;
 	});
-	return result;
+	list.splice(taskIndex, 1);
+	list.forEach(function (item, index) {
+		return item.id = index + 1;
+	});
+};
+deleteTask("wake up");
+console.log(list); */
+
+
+function addTask(idValue, taskValue, statusValue, priorityValue, positionValue) {
+	list.splice(positionValue = idValue - 1, 0, { id: idValue, task: taskValue, status: statusValue, priority: priorityValue, });
+	list.forEach(function (item, index) {
+		return item.id = index + 1;
+	});
 };
 
-console.log(deleteTask("have breakfast"));
+addTask(6, "go to sleep", TODO, HIGH_PRIORITY);
 console.log(list);
+
+
+/* function changeStatus(task, status) {
+	let taskIndex = list.findIndex(function (item) {
+		return item.task === task;
+	});
+	list.splice(taskIndex, 1);
+	list.forEach(function (item, index) {
+		return item.id = index + 1;
+	});
+};
+deleteTask("wake up");
+console.log(list); */

ddb7279867ff27c58d9b4c97677234276e06db90
Vaceslav Ignatuk <vaceslavignatuk@gmail.com>
2021-11-15 21:33:00 +0300

    ToDo refactoring

--- script.js
+++ script.js
@@ -3,38 +3,37 @@
 const DONE = "Done";
 const IN_PROGRESS = "In progress";
 const TODO = "ToDo";
-const LOW_PRIORITY = "Low";
-const HIGH_PRIORITY = "High";
+const LOW_PRIORITY = "Low priority";
+const HIGH_PRIORITY = "High priority";
 
 const list = [
-	{ id: 1, task: "wake up", status: DONE, priority: HIGH_PRIORITY, },
-	{ id: 2, task: "take a shower", status: DONE, priority: LOW_PRIORITY, },
-	{ id: 3, task: "have breakfast", status: DONE, priority: LOW_PRIORITY, },
-	{ id: 4, task: "learn JavaScript", status: TODO, priority: HIGH_PRIORITY, },
-	{ id: 5, task: "have a walk", status: TODO, priority: LOW_PRIORITY, },
-	{ id: 6, task: "watch stream", status: TODO, priority: HIGH_PRIORITY, },
+	{ task: "wake up", status: DONE, priority: HIGH_PRIORITY, },
+	{ task: "take a shower", status: DONE, priority: LOW_PRIORITY, },
+	{ task: "have breakfast", status: DONE, priority: LOW_PRIORITY, },
+	{ task: "learn JavaScript", status: TODO, priority: HIGH_PRIORITY, },
+	{ task: "have a walk", status: TODO, priority: LOW_PRIORITY, },
+	{ task: "watch stream", status: TODO, priority: HIGH_PRIORITY, },
 ];
+
+let countID = 0;
 
 function deleteTask(task) {
 	let taskIndex = list.findIndex(function (item) {
 		return item.task === task;
 	});
 	list.splice(taskIndex, 1);
-	list.forEach(function (item, index) {
-		return item.id = index + 1;
-	});
 };
 deleteTask("wake up");
 console.log(list);
 
 
-function addTask(taskValue, statusValue, priorityValue, positionValue) {
-	list.splice(positionValue, 0, { id: list.length + 1, task: taskValue, status: statusValue, priority: priorityValue, });
-	list.forEach(function (item, index) {
-		return item.id = index + 1;
+function addTask(taskValue, statusValue, priorityValue) {
+	list.push({ task: taskValue, status: statusValue, priority: priorityValue, });
+	list.forEach(function (item) {
+		return item.id = ++countID;
 	});
 };
-addTask("go to sleep", TODO, HIGH_PRIORITY, 6);
+addTask("go to sleep", TODO, HIGH_PRIORITY);
 console.log(list);
 
 
@@ -48,24 +47,41 @@ function changeStatus(task, status) {
 changeStatus("learn JavaScript", IN_PROGRESS);
 console.log(list);
 
-function taskStatus(status) {
+
+function showTasksByState(state) {
 	let isPrinted = false;
-	console.log(status + ":");
-	list.find(function (item) {
-		if (item.status === status) {
+	console.log(state + ":");
+	list.filter(function (item) {
+		const stateValue = item.priority === state || item.status === state;
+		if (stateValue) {
 			console.log(item.task);
 			isPrinted = true;
 		};
 	});
 	if (!isPrinted) {
 		console.log('-');
-	}
+	};
 };
 
-function showList() {
-	taskStatus(TODO);
-	taskStatus(IN_PROGRESS);
-	taskStatus(DONE);
+
+function showList(showBy) {
+	switch (showBy) {
+		case "status":
+			showTasksByState(TODO);
+			showTasksByState(IN_PROGRESS);
+			showTasksByState(DONE);
+			break;
+		case "priority":
+			showTasksByState(HIGH_PRIORITY);
+			showTasksByState(LOW_PRIORITY);
+			break;
+		default:
+			showTasksByState(TODO);
+			showTasksByState(IN_PROGRESS);
+			showTasksByState(DONE);
+			showTasksByState(HIGH_PRIORITY);
+			showTasksByState(LOW_PRIORITY);
+	};
 };
 
-showList();
+showList("priority");
